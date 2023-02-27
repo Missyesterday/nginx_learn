5bec1582f2f0b50c1aeb207cc60868228f4d44a0
Missyesterday <hyf1204@icloud.com>
2023-02-27 22:51:52 +0800

    添加信号量通信

--- nginx.c
+++ nginx.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
 int main(int argc, char* const* argv)
 {
     printf("你好, 世界!\n");
+    
+    //忽略SIGHUP信号,设置某个信号来的时候处理程序。
+    //SIG_IGN: 要求忽略这个信号，请操作系统不要用缺省的方式对待本进程（不要杀掉我）
+    signal(SIGHUP, SIG_IGN);
+    for (;;)
+    {
+        sleep(1);
+        printf("休息一秒\n");
+    }
+    
+    printf("程序退出,再见 !");
+    return 0;
+
 }
